e5a28a77768f10a6af56128e2a76e68a36531384
Laurent Thomas <laurent132.thomas@laposte.net>
2020-11-17 11:59:27 +0100

    import export functional

--- Fiji.app/jars/Lib/RoiGroupTable/RoiGroupTable.py
+++ Fiji.app/jars/Lib/RoiGroupTable/RoiGroupTable.py
@@ -1,142 +1,202 @@
-from javax.swing import JButton, JTable, JScrollPane, JPanel, JLabel, SpinnerNumberModel, JSpinner, JTextField
-from java.awt import GridLayout, Panel, Dimension
-from java.awt.event import ActionEvent, ActionListener
-from ij.gui import GenericDialog, Roi
-from RoiGroupTableModel import TableModel
+"""
+TO DO:
+- Add persistence of last directory
+"""
+from javax.swing             import JButton, JTable, JScrollPane, JPanel, JLabel, SpinnerNumberModel, JSpinner, JTextField, JFileChooser
+from javax.swing.filechooser import FileNameExtensionFilter
+from java.awt       import GridLayout, Panel, Dimension 
+from java.awt.event import ActionEvent, ActionListener 
+from java.io import File
+from ij.gui import GenericDialog, Roi 
+from ij import IJ
+from RoiGroupTableModel import TableModel 
+#from RoiGroupTable.RoiGroupTableModel import TableModel # for local tests 
+ 
+class AddButton(JButton, ActionListener):  
+	"""Class defining action of button : Adding a row to table"""
+ 
+	def __init__(self, groupTable):  
+		super(AddButton, self).__init__("Add row") 
+		self.addActionListener(self)  
+		self.groupTable = groupTable 
+ 
+ 
+	def actionPerformed(self, event):  
+		 
+		# Get group name  
+		groupName = self.groupTable.getNameField()   
+		   
+		# Check if group number already in table   
+		tableModel = self.groupTable.tableModel  
+		 
+		n = tableModel.getRowCount() 
+		tableModel.addRow(n+1, groupName) 
+		 
+  
  
 
-class AddButton(JButton, ActionListener): 
+class DeleteButton(JButton, ActionListener):  
 	"""Class defining action of button : Adding a row to table"""
-
-	def __init__(self, groupTable): 
-		super(AddButton, self).__init__("Add row")
-		self.addActionListener(self) 
-		self.groupTable = groupTable
-
-
-	def actionPerformed(self, event): 
-		
-		# Get group name 
-		groupName = self.groupTable.getNameField()  
-		  
-		# Check if group number already in table  
+	 
+	def __init__(self, groupTable):	  
+		super(DeleteButton, self).__init__("Delete last row") 
+		self.addActionListener(self)  
+		self.groupTable = groupTable 
+	 
+	def actionPerformed(self, event):  
+		"""Delete the last row""" 
 		tableModel = self.groupTable.tableModel 
-		
-		n = tableModel.getRowCount()
-		tableModel.addRow(n+1, groupName)
-		
- 
+		nRows = tableModel.getRowCount() 
+		if nRows>0: tableModel.deleteRow(nRows-1) 
 
 
-class DeleteButton(JButton, ActionListener): 
-	"""Class defining action of button : Adding a row to table"""
+
+class ImportButton(JButton, ActionListener): 
+	"""Class defining action of button : importing mappings from a file"""
 	
 	def __init__(self, groupTable):	 
-		super(DeleteButton, self).__init__("Delete last row")
+		super(ImportButton, self).__init__("Import from a file")
 		self.addActionListener(self) 
 		self.groupTable = groupTable
 	
 	def actionPerformed(self, event): 
 		"""Delete the last row"""
+		path = IJ.getFilePath("Select text file with group name mappings")
+		if not path: return 
+
+		# Read comma-separated group from file
+		with open(path, "r") as textFile:
+			stringGroup = textFile.readline().rstrip()
+
+		# Update table with content of file
 		tableModel = self.groupTable.tableModel
-		nRows = tableModel.getRowCount()
-		if nRows>0: tableModel.deleteRow(nRows-1)
- 
- 
- 
-class Table(Panel): 
-	"""
-	Implement a table with 2 columns: Roi group number and associated name. 
-	The table exposes the 
-	- tableModel: the raw data
-	- table: a JTable which takes care of the visualization/interactions (clicks...) 
-	"""
-	 
-	def __init__(self): 
-		 
-		super(Table, self).__init__(GridLayout(0,1)) # 1 column, as many rows as necessary
-		 
-		self.tableModel = TableModel()
-		self.table = JTable(self.tableModel) 
-		self.table.setPreferredScrollableViewportSize( Dimension(500, 70) ) 
-		self.table.setFillsViewportHeight(True) 
-		 
-		# Handle row selection 
-		#table.getSelectionModel().addListSelectionListener( RowListener()) 
-		#table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION) 
-		self.table.setRowSelectionAllowed(True) 
- 
- 
-		#Create the scroll pane and add the table to it. 
-		scrollPane = JScrollPane(self.table) 
-		#ScrollPane scrollPane =  ScrollPane() 
-		#scrollPane.add(table) 
-		 
-		#Add the scroll pane to self panel. 
-		self.add(scrollPane) 
-		 
-		 
-		# LABEL PANEL 
-		labelPanel = JPanel( GridLayout(0,1) )
-		#Panel labelPanel =  Panel( GridLayout(0,2)) 
-		#Panel labelPanel =  Panel() # looks bad when resizing 
-		 
-		# Add label  
-		labelName = JLabel("Name") 
-		labelPanel.add(labelName) 
-		self.add(labelPanel) 
-		 
-		 
-		# BUTTON PANNEL 
-		buttonPanel = JPanel( GridLayout(0,2) ) 
-		#Panel buttonPanel =  Panel() 
-		 
-		 
-		# Add text field for group name 
-		self.groupField = JTextField("new group") 
-		buttonPanel.add(self.groupField) 
+		tableModel.setGroupColumn(stringGroup)
+
+
+class ExportButton(JButton, ActionListener): 
+	"""Class defining action of button : exporting mappings to a file"""
+	
+	def __init__(self, groupTable):	 
+		super(ExportButton, self).__init__("Export to a file")
+		self.addActionListener(self) 
+		self.groupTable = groupTable
+	
+	def actionPerformed(self, event): 
+		"""Delete the last row"""
+		'''
+		path = IJ.getFilePath("Select text file to save group name mappings")
+		if not path: return 
+		'''
+		# Generate a save dialog
+		dialog = JFileChooser()
+		dialog.setSelectedFile( File("roiGroups.txt") )
+		dialog.setFileFilter( FileNameExtensionFilter("Text file",["txt"]) )
 		
+		output = dialog.showSaveDialog(self.groupTable) # could be argument None too
 		
-		# Button "Add Row" 
-		#JButton buttonAdd =  AddButton() 
-		buttonPanel.add( AddButton(self) ) 
+		if output in [JFileChooser.CANCEL_OPTION, JFileChooser.ERROR_OPTION]: return
+		filePath = dialog.getSelectedFile().getPath()
+		if not ("." in filePath): filePath += ".txt" # Add extension if missing
+
+		# Write the groupString to the file
+		groupString = self.groupTable.tableModel.getGroupString()
+		with open(filePath, "w") as textFile: 
+			textFile.write(groupString)
+			
+  
+class Table(Panel):  
+	"""
+	Implement a table with 2 columns: Roi group number and associated name.  
+	The table exposes the  
+	- tableModel: the raw data 
+	- table: a JTable which takes care of the visualization/interactions (clicks...)  
+	"""
+	  
+	def __init__(self):  
+		  
+		super(Table, self).__init__(GridLayout(0,1)) # 1 column, as many rows as necessary 
+		  
+		self.tableModel = TableModel() 
+		self.table = JTable(self.tableModel)  
+		self.table.setPreferredScrollableViewportSize( Dimension(500, 70) )  
+		self.table.setFillsViewportHeight(True)  
+		  
+		# Handle row selection  
+		#table.getSelectionModel().addListSelectionListener( RowListener())  
+		#table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION)  
+		self.table.setRowSelectionAllowed(True)  
+  
+  
+		#Create the scroll pane and add the table to it.  
+		scrollPane = JScrollPane(self.table)  
+		#ScrollPane scrollPane =  ScrollPane()  
+		#scrollPane.add(table)  
+		  
+		#Add the scroll pane to self panel.  
+		self.add(scrollPane)  
+		  
+		  
+		# LABEL PANEL  
+		labelPanel = JPanel( GridLayout(0,1) ) 
+		#Panel labelPanel =  Panel( GridLayout(0,2))  
+		#Panel labelPanel =  Panel() # looks bad when resizing  
+		  
+		# Add label   
+		labelName = JLabel("Name")  
+		labelPanel.add(labelName)  
+		self.add(labelPanel)  
+		  
+		  
+		# BUTTON PANNEL  
+		buttonPanel = JPanel( GridLayout(0,2) )  
+		#Panel buttonPanel =  Panel()  
+		  
+		  
+		# Add text field for group name  
+		self.groupField = JTextField("new group")  
+		buttonPanel.add(self.groupField)  
+		 
+		 
+		# Button "Add Row"  
+		#JButton buttonAdd =  AddButton()  
+		buttonPanel.add( AddButton(self) )  
 		buttonPanel.add( JLabel() ) # empty JLabel to fill the blank
-		buttonPanel.add( DeleteButton(self) ) 
+		buttonPanel.add( DeleteButton(self) )  
 		buttonPanel.add( ImportButton(self) ) 
 		buttonPanel.add( ExportButton(self) )
+		  
+		# Finally add button panel to main panel  
+		self.add(buttonPanel)  
+ 
+ 
+	def getNameField(self): 
+		"""Read the current state of the group name""" 
+		return self.groupField.getText() 
 		 
-		# Finally add button panel to main panel 
-		self.add(buttonPanel) 
-
-
-	def getNameField(self):
-		"""Read the current state of the group name"""
-		return self.groupField.getText()
-		
-		 
-	def showTable(self): 
+		  
+	def showTable(self):  
 		"""
-		Add the main panel to a GenericDialog and show it
+		Add the main panel to a GenericDialog and show it 
 		"""
-		gd = GenericDialog("Roi-group table") 
-		gd.addPanel(self) # Add current table instance to panel 
-		gd.showDialog() 
-		 
-		if gd.wasOKed(): 
-			# Update ImageJ Roi group names mapping
+		gd = GenericDialog("Roi-group table")  
+		gd.addPanel(self) # Add current table instance to panel  
+		gd.showDialog()  
+		  
+		if gd.wasOKed():  
+			# Update ImageJ Roi group names mapping 
 			stringGroup = self.tableModel.getGroupString()
-			Roi.setGroupNames(stringGroup) 
-	
-	
-	
-	def getTableModel(self): 
-		return self.tableModel
-
-
-if __name__ in ['__builtin__', '__main__']:
+			Roi.setGroupNames(stringGroup)  
+	 
+	 
+	 
+	def getTableModel(self):  
+		return self.tableModel 
+ 
+ 
+if __name__ in ['__builtin__', '__main__']: 
 	"""
-	Initialize a RoiGroupTable and show it.
+	Initialize a RoiGroupTable and show it. 
 	"""
-	table = Table()
-	
+	table = Table() 
+	 
 	table.showTable()
